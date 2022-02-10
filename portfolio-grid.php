8229498a9b39a86a304de9c6686cd320e96089f9
Ayushi Bharti <83293737+ayushi260395@users.noreply.github.com>
2022-02-10 15:26:45 +0530

    Add files via upload

--- portfolio-grid.php
+++ portfolio-grid.php
@@ -787,6 +787,11 @@
         </div>
    </div>
    </center>
+   
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
 
   <!-- ========== END FOOTER ========== -->
 
